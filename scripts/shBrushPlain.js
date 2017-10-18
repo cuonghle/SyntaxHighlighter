7383cfdafff8ddfbfacab93b8d4221a47717ac98
hcuongvn <cuonghl@marvell.com>
2017-10-18 19:12:14 +0700

    Fix bug

--- scripts/shBrushPlain.js
+++ scripts/shBrushPlain.js
@@ -28,7 +28,7 @@
 			{ regex: SyntaxHighlighter.regexLib.doubleQuotedString,		css: 'string' },			// strings
 			{ regex: SyntaxHighlighter.regexLib.singleQuotedString,		css: 'string' },			// strings
 			{ regex: /^ *#.*/gm,										css: 'preprocessor' },
-			{ regex: new RegExp(this.getKeywords(keywords), 'gm'),		css: 'keyword bold' }
+			{ regex: new RegExp(this.getKeywords(keywords), 'gm'),		css: 'keyword' }
 			];
 	};
 
